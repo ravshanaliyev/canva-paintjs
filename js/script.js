57be53400c66bc2e7b918a8379de4c3c48279021
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-17 12:26:33 +0500

    brush width added

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector("canvas");
 
 let contex = canvas.getContext("2d");
 let isDrawing = false;
+let brushWidth = 100;
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
@@ -15,6 +16,7 @@ const drawing = (e) => {
   if (!isDrawing) return;
   contex.lineTo(e.offsetX, e.offsetY);
   contex.stroke();
+  contex.lineWidth = brushWidth;
 };
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mouseup", () => (isDrawing = false));
