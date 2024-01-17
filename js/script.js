cc0e008d690aa8dd7f9f69ac2a9b16b5626b4e83
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-17 12:20:13 +0500

    start and end drawing functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 const canvas = document.querySelector("canvas");
 
 let contex = canvas.getContext("2d");
+let isDrawing = false;
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 });
+const startDraw = (e) => {
+  isDrawing = true;
+  contex.beginPath();
+  contex.moveTo(e.offsetX, e.offsetY);
+};
 const drawing = (e) => {
+  if (!isDrawing) return;
   contex.lineTo(e.offsetX, e.offsetY);
   contex.stroke();
 };
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mousemove", drawing);
